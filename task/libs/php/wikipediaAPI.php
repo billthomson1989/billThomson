72e1f939f55b845e535fd6876c901042a687fc24
billthomson1989 <bill-thomson@hotmail.co.uk>
2023-01-18 18:03:03 +0000

    updating the php files

--- task/libs/php/wikipediaAPI.php
+++ task/libs/php/wikipediaAPI.php
@@ -1,22 +1,13 @@
 <?php
-<<<<<<< HEAD
 header('Content-Type: application/json');
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 $url = "http://api.geonames.org/wikipediaSearch?q=london&maxRows=10&username=billthomson1989&type=json";
-=======
-
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
-$url = "http://api.geonames.org/wikipediaSearch?q=london&maxRows=10&username=billthomson1989";
->>>>>>> 1042dbd224e5accf92dc80fa478a71411b21469c
 
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-<<<<<<< HEAD
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $url);
 
@@ -27,16 +18,3 @@ $decoded = json_decode($result, true);
 
 echo json_encode($decoded);
 
-=======
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-curl_setopt($ch, CURLOPT_URL, $url);
-
-$result = curl_exec($ch);
-
-curl_close($ch);
- 
-$decode = json_decode($result, true);
-echo json_encode($decode);
->>>>>>> 1042dbd224e5accf92dc80fa478a71411b21469c
