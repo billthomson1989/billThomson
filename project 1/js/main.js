3cdbee6c9db0189961aed3f7aa07e47a3e38636f
billthomson1989 <bill-thomson@hotmail.co.uk>
2023-03-28 18:35:14 +0100

    fixed preloader

--- project 1/js/main.js
+++ project 1/js/main.js
@@ -244,14 +244,16 @@ function preloadImages(urls, allImagesLoadedCallback){
     var loadedCounter = 0;
     var toBeLoadedNumber = urls.length;
     urls.forEach(function(url){
-        preloadImage(url, function(){
-            loadedCounter++;
-            console.log('Number of loaded images: ' + loadedCounter);
-            if(loadedCounter == toBeLoadedNumber){
-                allImagesLoadedCallback();
-            }
-        });
-    });
+      if (url !== undefined) {
+          preloadImage(url, function(){
+              loadedCounter++;
+              console.log('Number of loaded images: ' + loadedCounter);
+              if(loadedCounter == toBeLoadedNumber){
+                  allImagesLoadedCallback();
+              }
+          });
+      }
+  });
     function preloadImage(url, anImageLoadedCallback){
         var img = new Image();
         img.onload = anImageLoadedCallback;
